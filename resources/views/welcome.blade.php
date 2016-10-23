e9621942cff39c6d42a27ac985b0dccbc693dcd1
peter279k <peter279k@gmail.com>
2016-10-23 11:27:39 +0800

    fix the static file path

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
             <!--Import Google Icon Font-->
             <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
             <!--Import materialize.css-->
-            <link rel="stylesheet" href="/bower_components/materialize/dist/css/materialize.css"  media="screen,projection"/>
-            <link rel="stylesheet" href="/css/app.css"/>
-            <link rel="shortcut icon" href="/css/favicon.ico">
+            <link rel="stylesheet" href="bower_components/materialize/dist/css/materialize.css"  media="screen,projection"/>
+            <link rel="stylesheet" href="css/app.css"/>
+            <link rel="shortcut icon" href="css/favicon.ico">
 
             <!--Let browser know website is optimized for mobile-->
             <meta charset="utf-8"/>
@@ -151,9 +151,9 @@
                 </div>
             </footer>
             <!--Import jQuery before materialize.js-->
-            <script src="/bower_components/jquery/dist/jquery.min.js"></script>
-            <script src="/bower_components/materialize/dist/js/materialize.min.js"></script>
-            <script src="/bower_components/html2canvas/build/html2canvas.min.js"></script>
-            <script src="/js/app.js"></script>
+            <script src="bower_components/jquery/dist/jquery.min.js"></script>
+            <script src="bower_components/materialize/dist/js/materialize.min.js"></script>
+            <script src="bower_components/html2canvas/build/html2canvas.min.js"></script>
+            <script src="js/app.js"></script>
     </body>
 </html>
